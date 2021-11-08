f8c937e48369f4c9af9051d4dfda4c9368fff04d
Peter Alfred Østergaard <palfred@rheasoft.dk>
2021-11-08 14:04:42 +0100

    Removed dependencies from JCenter/binfrog. Next step upgrade dependencies, and when working upgrade gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,16 +6,11 @@ group = "dk.rheasoft"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    jcenter()
-    maven {
-        setUrl("https://kotlin.bintray.com/js-externals")
-    }
+    mavenCentral()
 }
 
 dependencies {
     implementation(kotlin("stdlib-js"))
-    implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.6.12")
-    implementation("kotlin.js.externals:kotlin-js-jquery:2.0.0-0")
 }
 
 kotlin.target {
