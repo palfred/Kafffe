d28b22f96d5c595732279ccf184324ae686a7a84
Peter Alfred Østergaard <palfred@rheasoft.dk>
2021-11-24 15:04:28 +0100

    Upgrade gradle og kotlin - fjernet npm afhængighed

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+
 plugins {
     kotlin("js")
 }
@@ -9,40 +10,15 @@ repositories {
     mavenCentral()
 }
 
+val kotlinVersion: String by project
 dependencies {
-    implementation(kotlin("stdlib-js"))
+    implementation(kotlin("stdlib-js", kotlinVersion))
 }
 
-kotlin.target {
-    browser {}
-
-    compilations.all {
-        kotlinOptions {
-            metaInfo = true
-            sourceMap = true
-            sourceMapEmbedSources = "always"
+kotlin {
+    js {
+        browser {
         }
+        binaries.executable()
     }
 }
-
-// do not know how to get browserRun adn browser Webpack to work properly 2019.11.12
-// kotlin.target.browser { }
-
-    val assembleWeb by tasks.creating(Copy::class) {
-        dependsOn("build")
-        configurations["compileClasspath"].forEach {
-            println(it.absolutePath)
-            from(zipTree(it.absolutePath)) {
-                includeEmptyDirs = false
-                include("**/*")
-                exclude("**/*.kotlin_metadata")
-                // exclude("**/*.kjsm")
-                exclude("/META-INF/**")
-                include("/META-INF/resources/**")
-            }
-        }
-        from(zipTree("build/libs/Kafffe-${version}.jar")) {
-            include("**/*")
-        }
-        into("${buildDir}/web")
-    }
