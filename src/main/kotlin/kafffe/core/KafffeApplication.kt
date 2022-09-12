c22a9318f1fcbf3f9be5b7da1a265521c1ba2904
palfred <palfred@rheasoft.dk>
2022-09-12 16:21:27 +0200

    SoMe markdown input

---
+++ src/main/kotlin/kafffe/core/KafffeApplication.kt
@@ -0,0 +1,47 @@
+package kafffe.core
+
+import kotlinx.browser.document
+import kotlinx.browser.window
+
+/**
+ * This application object gives access to "global" function on the HTML document.
+ */
+object KafffeApplication {
+    /** The current rootcomponent allows access with out traversing parent components, which may not yet be established */
+    private val scriptIds = mutableSetOf<String>()
+    private val cssIds = mutableSetOf<String>()
+
+    fun disableHistoryBackward() {
+        window.history.pushState(null, document.title, window.location.href);
+        window.onpopstate = { window.history.pushState(null, document.title, window.location.href) }
+    }
+
+    fun addCss(ident: String, href_: String) {
+        if (cssIds.contains(ident)) return
+        KafffeHtml(document.head!!).apply {
+            link {
+                with(element!!) {
+                    id = ident
+                    href = href_
+                    type = "text/css"
+                    rel = "Stylesheet"
+                }
+            }
+            cssIds.add(ident)
+        }
+    }
+
+    fun addJsScriptRef(ident: String, href: String) {
+        if (scriptIds.contains(ident)) return
+        KafffeHtml(document.head!!).apply {
+            script {
+                with(element!!) {
+                    src = "$href#${ident}"
+                    type = "text/javascript"
+                }
+            }
+        }
+        scriptIds.add(ident)
+    }
+
+}
--- src/main/kotlin/kafffe/core/RootComponent.kt
+++ src/main/kotlin/kafffe/core/RootComponent.kt
@@ -4,45 +4,9 @@ import kotlinx.browser.document
 import kotlinx.browser.window
 
 /**
- * A root component that roughly corresponds to a HTML page, or could be used as base for SinglePage APP.
- * Can be used to control included Scripts and CSS.
+ * A root component that attach and detach it self to the HTML body
  */
 open class RootComponent : KafffeComponent() {
-    private val scriptIds = mutableSetOf<String>()
-    private val cssIds = mutableSetOf<String>()
-
-    fun disableHistoryBackward() {
-        window.history.pushState(null, document.title, window.location.href);
-        window.onpopstate = { window.history.pushState(null, document.title, window.location.href) }
-    }
-
-    fun addCss(ident: String, href_: String) {
-        if (cssIds.contains(ident)) return
-        KafffeHtml(document.head!!).apply {
-            link {
-                with(element!!) {
-                    id = ident
-                    href = href_
-                    type = "text/css"
-                    rel = "Stylesheet"
-                }
-            }
-            cssIds.add(ident)
-        }
-    }
-
-    fun addJsScriptRef(ident: String, href: String) {
-        if (scriptIds.contains(ident)) return
-        KafffeHtml(document.head!!).apply {
-            script {
-                with(element!!) {
-                    src = "$href#${ident}"
-                    type = "text/javascript"
-                }
-            }
-        }
-        scriptIds.add(ident)
-    }
 
     /**
      * Attaches the html to the Browser DOM = Show the HTML of this RootComponent
