7a8f5dfcfd1af452dfec488384fac6306ba0b683
palfred <palfred@rheasoft.dk>
2023-01-04 10:06:28 +0100

    data-visualisation#25. Kafffe pubslish as library in GitHub maven packages.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,18 @@
 
 plugins {
     kotlin("js")
+    id("maven-publish")
 }
 
 group = "dk.rheasoft"
-version = "1.0-SNAPSHOT"
+version = "1.10-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 val kotlinVersion: String by project
+println("kotlinVersion $kotlinVersion")
 dependencies {
     implementation(kotlin("stdlib-js", kotlinVersion))
 }
@@ -20,5 +22,46 @@ kotlin {
         browser {
         }
         binaries.executable()
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "CS_Aware_Next"
+            url = uri("https://maven.pkg.github.com/cs-aware-next/data-visualisation") // Github Package
+            credentials {
+                val githubUser: String by project
+                val githubToken: String by project
+                username = githubUser
+                password = githubToken
+
+            }
+        }
+        maven {
+            name = "Kafffe"
+            url = uri("https://maven.pkg.github.com/palfred/kafffe") // Github Package
+            credentials {
+                val githubUser: String by project
+                val githubToken: String by project
+                username = githubUser
+                password = githubToken
+
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("kotlin") {
+            from(components["kotlin"])
+
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            artifact(tasks.getByName<Zip>("kotlinSourcesJar"))
+
+            //configurePom(project)
+        }
     }
 }
