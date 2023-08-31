f317fc05f03f0af95c0190c27161de257bb90790
Peter Alfred Østergaard <palfred@rheasoft.dk>
2023-08-31 12:23:23 +0200

    Kotlin update

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 group = "dk.rheasoft"
-val workingOnVersion = "1.4"
+val workingOnVersion = "1.5"
 
 // Setup SNAPSHOT version if not on master branch.
 val githubBranch: String? by project
