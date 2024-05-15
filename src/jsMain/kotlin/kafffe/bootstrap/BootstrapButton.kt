56ec4e0c84ec02679ae095e473552cb4abb0b7bd
Peter Alfred Østergaard <palfred@rheasoft.dk>
2024-05-15 10:59:21 +0200

    Busy indicator button

--- src/jsMain/kotlin/kafffe/bootstrap/BootstrapButton.kt
+++ src/jsMain/kotlin/kafffe/bootstrap/BootstrapButton.kt
@@ -5,7 +5,8 @@ import kafffe.core.modifiers.CssClassModifier
 import org.w3c.dom.HTMLButtonElement
 import org.w3c.dom.HTMLElement
 
-open class BootstrapButton(label: Model<String>, val onClick: (source: BootstrapButton) -> Unit) : KafffeComponentWithModel<String>(label) {
+open class BootstrapButton(label: Model<String>, val onClick: (source: BootstrapButton) -> Unit) :
+    KafffeComponentWithModel<String>(label) {
     init {
         modifiers.add(CssClassModifier("btn"))
     }
@@ -17,35 +18,76 @@ open class BootstrapButton(label: Model<String>, val onClick: (source: Bootstrap
     var iconBefore: Boolean by rerenderOnChange(false)
     var disabled: Boolean by rerenderOnChange(false)
 
+    /**
+     * Can be used to make the button show processing on click. The busy icons is #indicatorIconClasses and defaults to a spinner icon.
+     * If a busy indicator is used then the onclick function is repsponsible for calling #processingDone
+     */
+    var useBusyIndicator: Boolean by rerenderOnChange(false)
+    var indicatorLabel: String by rerenderOnChange("")
+    var indicatorIconClasses: String by rerenderOnChange("fas fa-spinner fa-spin")
+    private var processing: Boolean by rerenderOnChange(false)
+
+    /**
+     * Should be called to get back to a non processing state in case busy indicator is used.
+     */
+    fun processingDone() {
+        processing = false
+    }
+
+    /**
+     * Setup busy indator using icon, good canditates can be found here: [spinners](https://fontawesome.com/search?o=r&m=free&c=spinners&s=solid)
+     */
+    fun useFontAwesomeSpinner(fontAwesomeIcon: String) {
+        indicatorIconClasses = "fas $fontAwesomeIcon fa-spin"
+        useBusyIndicator = true
+    }
+
+    fun useSpinner() = useFontAwesomeSpinner("fa-spinner")
+    fun useSpinGear() = useFontAwesomeSpinner("fa-gear")
+
     override fun modifyHtml(element: HTMLElement): HTMLElement {
         color.btnModifier.modify(element)
         return super.modifyHtml(element)
     }
 
     override fun KafffeHtmlBase.kafffeHtml() =
-            button() {
-                withElement {
-                    onclick = { onClick(this@BootstrapButton) }
-                    type = btnType.toString()
-                    if (this@BootstrapButton.disabled) {
-                        addClass("disabled")
-                        disabled = true
-                    }
-                }
-                if (iconBefore && !iconClasses.isEmpty()) {
-                    icon()
-                    text(" ")
-                }
-                text(label)
-                if (!iconBefore && !iconClasses.isEmpty()) {
-                    text(" ")
-                    icon()
+        button() {
+            withElement {
+                onclick = { internalClick() }
+                type = btnType.toString()
+                if (this@BootstrapButton.disabled || processing) {
+                    addClass("disabled")
+                    disabled = true
                 }
             }
+            if (iconBefore && hasIcons()) {
+                icon()
+                text(" ")
+            }
+            text(label)
+            if (!iconBefore && hasIcons()) {
+                text(" ")
+                icon()
+            }
+        }
+
+    private fun hasIcons() = iconClasses.isNotEmpty() || (processing && indicatorIconClasses.isNotEmpty())
+
+    private fun internalClick() {
+        if (useBusyIndicator) {
+            processing = true
+        }
+        onClick(this@BootstrapButton)
+    }
 
     protected open fun KafffeHtml<HTMLButtonElement>.icon() {
         i {
-            iconClasses.split(' ').forEach { addClass(it) }
+            addClass(
+                when {
+                    processing -> indicatorIconClasses
+                    else -> iconClasses
+                }
+            )
         }
     }
 }
