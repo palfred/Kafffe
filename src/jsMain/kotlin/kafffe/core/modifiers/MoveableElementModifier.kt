914237363e8be188b9131697bfc0362ec4adeac5
Peter Alfred Østergaard <palfred@rheasoft.dk>
2025-01-29 10:55:04 +0100

    Moveable modal - some docs added

--- src/jsMain/kotlin/kafffe/core/modifiers/MoveableElementModifier.kt
+++ src/jsMain/kotlin/kafffe/core/modifiers/MoveableElementModifier.kt
@@ -28,6 +28,8 @@ class MoveableElementModifier(
     private lateinit var movingElement: HTMLElement
     private lateinit var handleElement: HTMLElement
 
+    // Event handlers defined as properties in order to able to use same value for removal of
+    // globally registered handlers.
     private val startDrag: (Event) -> Unit = { event ->
         if (event is MouseEvent) {
             isDragging = true
@@ -62,6 +64,8 @@ class MoveableElementModifier(
         movingElement = element
         currentX = 0
         currentY = 0
+
+        // get the element used for dragging handle, either by CSS class or the moving element directly.
         if (handleCssClass.isNotEmpty()) {
             val byClassName: HTMLCollection =
                 element.getElementsByClassName(handleCssClass)
@@ -70,6 +74,7 @@ class MoveableElementModifier(
         } else {
             handleElement = element
         }
+
         handleElement.style.cursor = "move"
         handleElement.addEventListener("mousedown", startDrag)
     }
